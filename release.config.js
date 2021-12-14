ccb406e96a1346be75947a60f2863348260aa422
Guilherme Tod <guilhermetod@gmail.com>
2021-12-14 16:56:22 -0300

    chore: test

--- release.config.js
+++ release.config.js
@@ -3,8 +3,11 @@ const { releaseRules } = require('./tools/utils/release-rules');
 module.exports = {
   branches: ['main'],
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules }],
-    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules: releaseRules.map(({ type, release }) => ({ type, release })) }],
+    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits',
+      presetConfig: {
+        types: releaseRules.map(({ type, section }) => ({ type, section, hidden: false })),
+      } }],
     ['@semantic-release/npm', { npmPublish: false }],
     ['@semantic-release/git'],
     '@semantic-release/github',
--- tools/utils/release-rules.js
+++ tools/utils/release-rules.js
@@ -1,15 +1,15 @@
 const releaseRules = [
-  { type: 'Build', release: 'patch' },
-  { type: 'Chore', release: false },
-  { type: 'CI', release: false },
-  { type: 'Docs', release: false },
-  { type: 'Feat', release: 'minor' },
-  { type: 'Fix', release: 'patch' },
-  { type: 'Perf', release: 'patch' },
-  { type: 'Refactor', release: 'patch' },
-  { type: 'Revert', release: 'patch' },
-  { type: 'Style', release: false },
-  { type: 'Test', release: false },
+  { type: 'Build', release: 'patch', section: 'Other Changes' },
+  { type: 'Chore', release: false, section: 'Other Changes' },
+  { type: 'CI', release: false, section: 'Other Changes' },
+  { type: 'Docs', release: false, section: 'Other Changes' },
+  { type: 'Feat', release: 'minor', section: 'Features' },
+  { type: 'Fix', release: 'patch', section: 'Bug fixes' },
+  { type: 'Perf', release: 'patch', section: 'Other Changes' },
+  { type: 'Refactor', release: 'patch', section: 'Other Changes' },
+  { type: 'Revert', release: 'patch', section: 'Other Changes' },
+  { type: 'Style', release: false, section: 'Other Changes' },
+  { type: 'Test', release: false, section: 'Other Changes' },
 ];
 
 module.exports = { releaseRules };
