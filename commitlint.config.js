70c20748af80e2bacf68962ae798600e167e15f7
Guilherme Tod <guilhermetod@gmail.com>
2021-12-14 14:21:41 -0300

    Fix: CI

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ const types = releaseRules.map((rule) => rule.type);
 
 module.exports = {
   extends: '@commitlint/config-conventional',
+  ignore: [(message) => message.includes('[skip ci]')],
   rules: {
     'type-case': [2, 'always', 'sentence-case'],
     'type-enum': [2, 'always', types],
