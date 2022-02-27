b8ec896d68023f969c919f6219d8b0c0b3418a3e
Guilherme Tod <guilhermetod@gmail.com>
2022-02-27 17:04:26 -0300

    CI: test without skip ci

--- release.config.js
+++ release.config.js
@@ -1,15 +1,12 @@
-const { releaseRules } = require('./tools/utils/release-rules');
+const { commitConvention } = require('./tools/utils/commit-convention');
 
 module.exports = {
   branches: ['main'],
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules: releaseRules.map(({ type, release }) => ({ type, release })) }],
-    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits',
-      presetConfig: {
-        types: releaseRules.map(({ type, section }) => ({ type, section, hidden: false })),
-      } }],
-    ['@semantic-release/npm', { npmPublish: false }],
-    ['@semantic-release/git'],
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', commitConvention }],
+    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+    '@semantic-release/npm',
+    ['@semantic-release/git', {message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}'}],
     '@semantic-release/github',
   ],
 };
