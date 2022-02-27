fbad0799703b7402f96ffd38915f17d373ebecb3
Guilherme Tod <guilhermetod@gmail.com>
2022-02-27 17:26:16 -0300

    Chore: fix commit convention

--- release.config.js
+++ release.config.js
@@ -1,9 +1,11 @@
 const { commitConvention } = require('./tools/utils/commit-convention');
 
+const releaseRules = commitConvention.map(({ type, release }) => ({ type, release }));
+
 module.exports = {
   branches: ['main'],
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules: commitConvention }],
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules }],
     ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
     ['@semantic-release/npm', { npmPublish: false }],
     // eslint-disable-next-line no-template-curly-in-string
