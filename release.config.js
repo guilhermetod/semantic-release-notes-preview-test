3192d505f4cc126127c55cd32e3d0c05d4cd78ef
Guilherme Tod <guilhermetod@gmail.com>
2022-02-27 17:53:46 -0300

    Feat: changelog

--- release.config.js
+++ release.config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits', releaseRules }],
     ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+    ['@semantic-release/changelog'],
     ['@semantic-release/npm', { npmPublish: false }],
     // eslint-disable-next-line no-template-curly-in-string
     ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}' }],
